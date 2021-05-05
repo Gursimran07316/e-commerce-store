e2115d70eaf403f7bb019e50ad342f85cf41e28d
Gursimran <jattgur7172@example.com>
2021-05-05 16:40:40 +0530

    product screen added

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -4,39 +4,39 @@ const Rating = ({ rating, reviews, color }) => {
   return (
     <>
       {rating >= 1 ? (
-        <i style={{ color }} class="fas fa-star"></i>
+        <i style={{ color }} className="fas fa-star"></i>
       ) : rating >= 0.5 ? (
-        <i style={{ color }} class="fas fa-star-half-alt"></i>
+        <i style={{ color }} className="fas fa-star-half-alt"></i>
       ) : (
-        <i style={{ color }} class="far fa-star"></i>
+        <i style={{ color }} className="far fa-star"></i>
       )}
       {rating >= 2 ? (
-        <i style={{ color }} class="fas fa-star"></i>
+        <i style={{ color }} className="fas fa-star"></i>
       ) : rating >= 1.5 ? (
-        <i style={{ color }} class="fas fa-star-half-alt"></i>
+        <i style={{ color }} className="fas fa-star-half-alt"></i>
       ) : (
-        <i style={{ color }} class="far fa-star"></i>
+        <i style={{ color }} className="far fa-star"></i>
       )}
       {rating >= 3 ? (
-        <i style={{ color }} class="fas fa-star"></i>
+        <i style={{ color }} className="fas fa-star"></i>
       ) : rating >= 2.5 ? (
-        <i style={{ color }} class="fas fa-star-half-alt"></i>
+        <i style={{ color }} className="fas fa-star-half-alt"></i>
       ) : (
-        <i style={{ color }} class="far fa-star"></i>
+        <i style={{ color }} className="far fa-star"></i>
       )}
       {rating >= 4 ? (
-        <i style={{ color }} class="fas fa-star"></i>
+        <i style={{ color }} className="fas fa-star"></i>
       ) : rating >= 3.5 ? (
-        <i style={{ color }} class="fas fa-star-half-alt"></i>
+        <i style={{ color }} className="fas fa-star-half-alt"></i>
       ) : (
-        <i style={{ color }} class="far fa-star"></i>
+        <i style={{ color }} className="far fa-star"></i>
       )}
       {rating >= 5 ? (
-        <i style={{ color }} class="fas fa-star"></i>
+        <i style={{ color }} className="fas fa-star"></i>
       ) : rating >= 4.5 ? (
-        <i style={{ color }} class="fas fa-star-half-alt"></i>
+        <i style={{ color }} className="fas fa-star-half-alt"></i>
       ) : (
-        <i style={{ color }} class="far fa-star"></i>
+        <i style={{ color }} className="far fa-star"></i>
       )}
       {reviews}
     </>
