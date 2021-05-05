0b2a14073b9787301019e8bc57260de1898c9a5e
Gursimran <jattgur7172@example.com>
2021-05-05 15:55:42 +0530

    bug fixes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
 import HomeScreen from "./screens/HomeScreen";
 import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProductList from "../components/ProductList";
+import products from "../products";
 
 const HomeScreen = () => {
   return (
