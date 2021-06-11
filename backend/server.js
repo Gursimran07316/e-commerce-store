61706f864984f89e3dd00073e7bf940a36712e06
Gursimran <jattgur7172@example.com>
2021-06-11 12:23:47 +0530

    bug fixes

--- backend/server.js
+++ backend/server.js
@@ -5,12 +5,10 @@ const orderRoutes = require("./routes/orderRoutes");
 const dotenv = require("dotenv");
 const path = require("path");
 const uploadRoutes = require("./routes/uploadRoutes");
-const cors = require("cors");
 const connectDB = require("./config/db");
 const app = express();
 dotenv.config();
 connectDB();
-app.use(cors());
 app.use(express.json());
 
 app.use("/api/products", productRoutes);
